58954e7d0898cc3249f2376ae44197429ce0603c
JiaShuo <js982986555@live.com>
2019-05-28 17:47:34 +0800

    update build config and add test

--- build.sbt
+++ build.sbt
@@ -1,13 +1,30 @@
-version := "0.0.4-SNAPSHOT"
+version := "1.11.4-1-SNAPSHOT"
 
-organization := "com.xiaomi"
+organization := "com.xiaomi.infra"
 
 name := "pegasus-scala-client"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.12.8"
+
+publishMavenStyle := true
+
+crossPaths := false
+
+resolvers ++= Seq( //额外仓库添加
+  "Admonitor Repository" at "http://your-url/",
+  "Local Maven Repository" at "file://your-url/"
+)
+
+publishTo := {//发布地址
+  val nexus = "http://your-url/"
+  if (isSnapshot.value) Some("snapshots" at nexus + "snapshots") else Some("releases" at nexus + "releases")
+}
+
+// credentials, 路径可以自己指定：
+credentials += Credentials(new File((Path.userHome / ".sbt" / ".credentials").toString()))
 
 libraryDependencies ++= Seq(
   "com.google.guava" % "guava" % "20.0",
-  "com.xiaomi.infra" % "pegasus-client" % "1.11.2-thrift-0.11.0-inlined",
+  "com.xiaomi.infra" % "pegasus-client" % "1.11.4-thrift-0.11.0-inlined",
   "org.scalatest" %% "scalatest" % "3.0.3" % Test
 )
--- src/test/scala/com/xiaomi/infra/pegasus/scalaclient/TableSpec.scala
+++ src/test/scala/com/xiaomi/infra/pegasus/scalaclient/TableSpec.scala
@@ -3,6 +3,8 @@ package com.xiaomi.infra.pegasus.scalaclient
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import Serializers._
 
+import scala.concurrent.duration.Duration
+
 /**
   * [Copyright]
   * Author: oujinliang
@@ -72,7 +74,106 @@ class TableSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
         }
     }
 
-    private def delHashKey[A](c: ScalaPegasusClient, table: String, hashKey: A)(implicit ser: Serializer[A]) = {
+    "client batch get/set/del" should "work" in {
+        withClient { c =>
+            val hashKey = "batch"
+
+            //test batchSet
+            val batchSet1 = new SetItem[String, String, String]("batch_1", "sort_1", "value_1", Duration.Zero)
+            val batchSet2 = new SetItem[String, String, String]("batch_2", "sort_2", "value_2", Duration.Zero)
+            c.batchSet(table, Seq(batchSet1, batchSet2))
+            c.exists(table, "batch_1", "sort_1") should equal(true)
+            c.exists(table, "batch_2", "sort_2") should equal(true)
+
+            //test batchSet2
+            val batchSet3 = new SetItem[String, String, String]("batch_3", "sort_3", "value_3", Duration.Zero)
+            val batchSet4 = new SetItem[String, String, String]("batch_4", "sort_4", "value_4", Duration.Zero)
+            c.batchSet2(table, Seq(batchSet3, batchSet4))
+            c.exists(table, "batch_3", "sort_3") should equal(true)
+            c.exists(table, "batch_4", "sort_4") should equal(true)
+
+            ////test batchMultitSet
+            val batchMultiSet1 = new HashKeyData[String, String, String]("batchMultiSet_1", List(("sort_1", "value_1"), ("sort_2", "value_2")))
+            val batchMultiSet2 = new HashKeyData[String, String, String]("batchMultiSet_2", List(("sort_1", "value_1"), ("sort_2", "value_2")))
+            c.batchMultitSet(table, Seq(batchMultiSet1, batchMultiSet2))
+            c.exists(table, "batchMultiSet_1", "sort_1") should equal(true)
+            c.exists(table, "batchMultiSet_1", "sort_2") should equal(true)
+            c.exists(table, "batchMultiSet_2", "sort_1") should equal(true)
+            c.exists(table, "batchMultiSet_2", "sort_2") should equal(true)
+
+            //test batchMultitSet2
+            val batchMultiSet3 = new HashKeyData[String, String, String]("batchMultiSet_3", List(("sort_1", "value_1"), ("sort_2", "value_2")))
+            val batchMultiSet4 = new HashKeyData[String, String, String]("batchMultiSet_4", List(("sort_1", "value_1"), ("sort_2", "value_2")))
+            c.batchMultitSet(table, Seq(batchMultiSet3, batchMultiSet4))
+            c.exists(table, "batchMultiSet_3", "sort_1") should equal(true)
+            c.exists(table, "batchMultiSet_3", "sort_2") should equal(true)
+            c.exists(table, "batchMultiSet_4", "sort_1") should equal(true)
+            c.exists(table, "batchMultiSet_4", "sort_2") should equal(true)
+
+            //test batchGet
+            val batchGet1 = new PegasusKey[String, String]("batch_1", "sort_1")
+            val batchGet2 = new PegasusKey[String, String]("batch_2", "sort_2")
+            val values1 = List("value_1", "value_2")
+            val res1 = c.batchGet(table, List(batchGet1, batchGet2)).as[String]
+            res1.indices.foreach { i => res1(i) should equal(values1(i)) }
+
+            //test batchGet2
+            val batchGet3 = new PegasusKey[String, String]("batch_3", "sort_3")
+            val batchGet4 = new PegasusKey[String, String]("batch_4", "sort_4")
+            val values2 = List("value_3", "value_4")
+            val res2 = c.batchGet(table, List(batchGet3, batchGet4)).as[String]
+            res2.indices.foreach { i => res2(i) should equal(values2(i)) }
+
+            //test batchMultiGet
+            val batchMultiGet1 = ("batchMultiSet_1", Seq("sort_1", "sort_2"))
+            val batchMultiGet2 = ("batchMultiSet_2", Seq("sort_1", "sort_2"))
+            val res3 = c.batchMultiGet(table, Seq(batchMultiGet1, batchMultiGet2))
+            val hashKeys1 = List("batchMultiSet_1", "batchMultiSet_2")
+            res3.indices.foreach { i => res3(i).hashKey should equal(hashKeys1(i)) }
+
+            //test batchMultiGet2
+            val batchMultiGet3 = ("batchMultiSet_3", Seq("sort_1", "sort_2"))
+            val batchMultiGet4 = ("batchMultiSet_4", Seq("sort_1", "sort_2"))
+            val res4 = c.batchMultiGet(table, Seq(batchMultiGet3, batchMultiGet4))
+            val hashKeys2 = List("batchMultiSet_3", "batchMultiSet_4")
+            res4.indices.foreach { i => res4(i).hashKey should equal(hashKeys2(i)) }
+
+            //test batchDel
+            val batchDel1 = new PegasusKey[String, String]("batch_1", "sort_1")
+            val batchDel2 = new PegasusKey[String, String]("batch_2", "sort_2")
+            c.batchDel(table, Seq(batchDel1, batchDel2))
+            c.exists(table, "batch_1", "sort_1") should equal(false)
+            c.exists(table, "batch_2", "sort_2") should equal(false)
+
+            //test batchDel2
+            val batchDel3 = new PegasusKey[String, String]("batch_3", "sort_3")
+            val batchDel4 = new PegasusKey[String, String]("batch_4", "sort_4")
+            c.batchDel(table, Seq(batchDel3, batchDel4))
+            c.exists(table, "batch_3", "sort_3") should equal(false)
+            c.exists(table, "batch_4", "sort_4") should equal(false)
+
+            //test batchMultiDel
+            val batchMultiDel1 = ("batchMultiSet_1", Seq("sort_1", "sort_2"))
+            val batchMultiDel2 = ("batchMultiSet_2", Seq("sort_1", "sort_2"))
+            c.batchMultiDel(table, Seq(batchMultiDel1, batchMultiDel2))
+            c.exists(table, "batchMultiSet_1", "sort_1") should equal(false)
+            c.exists(table, "batchMultiSet_1", "sort_2") should equal(false)
+            c.exists(table, "batchMultiSet_2", "sort_1") should equal(false)
+            c.exists(table, "batchMultiSet_2", "sort_2") should equal(false)
+
+            //test batchMultiDel2
+            val batchMultiDel3 = ("batchMultiSet_3", Seq("sort_1", "sort_2"))
+            val batchMultiDel4 = ("batchMultiSet_4", Seq("sort_1", "sort_2"))
+            c.batchMultiDel(table, Seq(batchMultiDel3, batchMultiDel4))
+            c.exists(table, "batchMultiSet_3", "sort_1") should equal(false)
+            c.exists(table, "batchMultiSet_3", "sort_2") should equal(false)
+            c.exists(table, "batchMultiSet_4", "sort_1") should equal(false)
+            c.exists(table, "batchMultiSet_4", "sort_2") should equal(false)
+        }
+    }
+
+
+  private def delHashKey[A](c: ScalaPegasusClient, table: String, hashKey: A)(implicit ser: Serializer[A]) = {
         val keys = c.multiGetSortKeys(table, hashKey)
         if (keys.values.nonEmpty) {
             c.multiDel(table, hashKey, keys.values)
